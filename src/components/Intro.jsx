6d4bc2ee663f5712473ec8a901e71fb839bfd350
nishant-thapa <itsnishantu@gmail.com>
2024-08-20 21:38:46 +0545

    updating the hovering effect-->making the coursor pointer:intor.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Intro() {
   return (
-    <div className='font-michroma ml-6 mt-4 font-bold'>
+    <div className='font-michroma ml-6 mt-4 font-bold cursor-pointer'>
         <h2 className='text-custom-font1'>Porsche Nepal</h2>
         <p className='h-[141px] w-[650px] text-custom-font2'>Porsche, a renowned luxury automobile manufacturer known for its high-performance sports cars, has made a notable entry into the Nepalese market. The brand's presence in Nepal reflects a growing appetite for premium vehicles among affluent customers in the country.</p>
         <button className='bg-black text-white h-[56px] w-[182px] rounded-3xl'>Explore</button>
